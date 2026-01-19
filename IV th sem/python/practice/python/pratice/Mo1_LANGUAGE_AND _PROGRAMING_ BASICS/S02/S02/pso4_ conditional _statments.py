dcfb2a4cd5baedf8cae0d2ebbea37e6bb43d7b0e
akhilesH-2-3 <2401100cse0157@cse.suh.edu.in>
2026-01-19 16:09:54 +0530

    commit message

--- IV th sem/python/practice/python/pratice/Mo1_LANGUAGE_AND _PROGRAMING_ BASICS/S02/S02/pso4_ conditional _statments.py
+++ IV th sem/python/practice/python/pratice/Mo1_LANGUAGE_AND _PROGRAMING_ BASICS/S02/S02/pso4_ conditional _statments.py
@@ -7,13 +7,16 @@ print('even' if check(n) else'odd' )
 # prime number 
 import math 
 def prime(n):
+    if n==2:
+      return True
     for i in range (2,int(math.sqrt(n))+1,1):
-        if n==2:
-            return True
+        # if n==2:
+        #     break
+        #     return True
         if n%i==0:
           break
           return False
-print("prime " if prime(n) else "not prime")      
+print("prime " if prime(n) else "not prime")       
 
 
 
