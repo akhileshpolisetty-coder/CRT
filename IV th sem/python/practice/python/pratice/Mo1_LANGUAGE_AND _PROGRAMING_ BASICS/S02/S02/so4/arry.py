c914c6bb6aeeffefdd533a2d77453f4b8fc25877
akhilesH-2-3 <2401100cse0157@cse.suh.edu.in>
2026-02-11 12:07:32 +0530

    mes

--- IV th sem/python/practice/python/pratice/Mo1_LANGUAGE_AND _PROGRAMING_ BASICS/S02/S02/so4/arry.py
+++
@@ -1,7 +0,0 @@
-import array
-arr=array.array('i',[])
-print(arr,type(arr))
-arr.append(10);arr.append(20)
-print(arr)
-s=input()
-print(sum(int(d) for d in s))
--- IV th sem/python/practice/python/pratice/demo.py
+++
@@ -1,2 +0,0 @@
-# cooment 
-print('sai ram')
