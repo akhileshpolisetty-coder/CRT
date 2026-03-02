de27776294f01d63cd6442cead0846db39158bfd
akhilesH-2-3 <2401100cse0157@cse.suh.edu.in>
2026-03-02 13:58:38 +0530

    message commited

---
+++ IV-SEM/Python/Assignments/AST03/task.py
@@ -0,0 +1,11 @@
+def Student_Grade_System(name:str,n1: int,n2: int,n3: int) -> str:
+   pass
+   avg = round((n1 + n2 + n3) / 3, 2)
+   status = "Pass" if min(n1, n2, n3) >= 35 else "Fail"
+   return f"Average grade: {avg}, Status: {status}"
+
+
+if __name__ == '__main__':
+    name = input()
+    n1,n2,n3 = list(map(int,input().split()))
+    print(Student_Grade_System(name,n1,n2,n3))
