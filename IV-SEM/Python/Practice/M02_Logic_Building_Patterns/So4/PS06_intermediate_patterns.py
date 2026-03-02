fbfaa04faf5c9cc3df0713b4cdd10cb1d7b21e59
akhilesH-2-3 <2401100cse0157@cse.suh.edu.in>
2026-03-02 11:39:36 +0530

    commit meaasage

--- IV-SEM/Python/Practice/M02_Logic_Building_Patterns/So4/PS06_intermediate_patterns.py
+++ IV-SEM/Python/Practice/M02_Logic_Building_Patterns/So4/PS06_intermediate_patterns.py
@@ -7,7 +7,24 @@ print(res)
 print([i for i in li if i%2==0])   
 lii=['a','b','c']
 print(''.join(lii))
-
 n = 4
+# Top half (Pyramid)
 for i in range(1, n+1):
     print('-' * (n-i) + '*' * (2*i-1))
+
+# Bottom half (Inverted Pyramid)
+for j in range(n-1, 0, -1):
+    print('-' * (n-j) + '*' * (2*j-1))
+
+n = 4
+for i in range(1, n + 1):
+   
+    print(" " * (n - i), end="")
+    
+
+    for j in range(1, i + 1):
+        print(chr(64+i), end=" ")
+    
+    print() # New line
+
+
